83cd92455abddfe0a22872bf447cce49bbd106dc
HenriqueVeloso99 <hsvrick30@gmail.com>
2026-03-08 00:01:11 -0300

    versao0.8

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const dados = {
+
+  fotos: [
+    {
+      img: "imagens/objetos/foto1.jpeg",
+      data: "12/03/2024",
+      local: "Praia",   
+      motivo: "Viagem",
+      texto: "Foi um dia muito bom."
+    },
+
+    {
+      img: "imagens/objetos/foto2.jpeg",
+      data: "12/03/2024",
+      local: "Praia",   
+      motivo: "Viagem",
+      texto: "Foi um dia muito bom."
+    }
+  ],
+
+  presentes: [
+    {
+      titulo: "Urso de pelúcia",
+      img: "imagens/presente1.png",
+      descricao: "Primeiro presente que você me deu."
+    }
+  ],
+
+  mapas: [
+    {
+      titulo: "Parque",
+      img: "imagens/mapa1.png",
+      descricao: "Lugar onde nos encontramos."
+    }
+  ],
+
+  cartas: [
+    {
+      titulo: "Carta 1",
+      texto: "Texto da carta..."
+    }
+  ]
+
+};
