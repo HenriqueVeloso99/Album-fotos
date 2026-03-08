9501b4e19fa21697528275568cbb8d7aba6af293
HenriqueVeloso99 <hsvrick30@gmail.com>
2026-03-08 10:06:14 -0300

    versao1.0

--- js/main.js
+++ js/main.js
@@ -3,42 +3,91 @@ const dados = {
   fotos: [
     {
       img: "imagens/objetos/foto1.jpeg",
-      data: "12/03/2024",
-      local: "Praia",   
-      motivo: "Viagem",
-      texto: "Foi um dia muito bom."
+      data: "07/02/2026",
+      local: "Panda cafeteria",   
+      motivo: "Pedido de Namoro",
+      texto: "Um dos dias mais felizes da minha vida, e que marcou a melhor fase dela, a fase que você está <3, pena que as empregadas atrapalharam."
     },
 
     {
       img: "imagens/objetos/foto2.jpeg",
-      data: "12/03/2024",
-      local: "Praia",   
-      motivo: "Viagem",
-      texto: "Foi um dia muito bom."
+      data: "07/02/2026",
+      local: "Be COffe",   
+      motivo: "1 mês de namoro",
+      texto: "Foi um dia incríiiivel, amo cada detalhe, melhor comemoração possível, mesmo que só tenha ganhado um brownie de 1cm quadrado "
+    },
+
+    {
+      img: "imagens/objetos/foto2.jpeg",
+      data: "07/02/2026",
+      local: "Be COffe",   
+      motivo: "1 mês de namoro",
+      texto: "Foi um dia incríiiivel, amo cada detalhe, melhor comemoração possível, mesmo que só tenha ganhado um brownie de 1cm quadrado "
+    },
+
+    {
+      img: "imagens/objetos/foto1.jpeg",
+      data: "07/02/2026",
+      local: "Be COffe",   
+      motivo: "1 mês de namoro",
+      texto: "Foi um dia incríiiivel, amo cada detalhe, melhor comemoração possível, mesmo que só tenha ganhado um brownie de 1cm quadrado "
     }
   ],
 
   presentes: [
     {
-      titulo: "Urso de pelúcia",
-      img: "imagens/presente1.png",
-      descricao: "Primeiro presente que você me deu."
-    }
+      titulo: "Origami 'Atreides '",
+      img: "imagens/objetos/gift1.jpeg",
+      descricao: "O primeiro presenta a mão que fiz, com muito carinho e esforço kkkkkk,era uma tentativa do símbolo da casa Atreides, mas saiu mais uma poma"
+    },
+
+    {
+      titulo: "Buquê de Tulipas",
+      img: "imagens/objetos/gift2.jpeg",
+      descricao: "Flores que eu levei no pedido de namoro, achei lindas na verdade, escolhi o lugar só pra estar perto de uma floricultura kkkkkkk, o primeiro buquê que dei a alguém"
+    },
+
+    {
+      titulo: "Carta Pokémon",
+      img: "imagens/objetos/gift3.jpeg",
+      descricao: "Ganhei o pacote de aniversário e queria te dar alguma, daqui a pouco a caminha explode de tanta coisa kkkkkkk"
+    },
+
+    {
+      titulo: "Colares Fúria da noite e da luz",
+      img: "imagens/objetos/gift4.jpeg",
+      descricao: "O presente mais legal eu acho, amei os colares, muito lindas, além de simbolizar nozes"
+    },
+
+    {
+      titulo: "Suas cartinhas",
+      img: "imagens/objetos/gift5.jpeg",
+      descricao: "Meu Deus eu real surtei internamente, ainda mais com os cookies, melhor presente do mundo da melhor namorada do mundo <3"
+    },
+
   ],
 
   mapas: [
     {
-      titulo: "Parque",
-      img: "imagens/mapa1.png",
-      descricao: "Lugar onde nos encontramos."
+      titulo: "Arrakis",
+      img: "imagens/objetos/mapa1.jpeg",
+      descricao: "O mapa de Arrakis, por incrivel que pareça fiz ele na terceira tentativa, gostei muito de fazer"
     }
   ],
 
   cartas: [
     {
-      titulo: "Carta 1",
-      texto: "Texto da carta..."
+      titulo: "Carta 1- Pedido",
+      img: "imagens/objetos/carta1.jpeg",
+      texto: "A carta do pedido de namoro, deu muito trabalho pra fazer mas fiquei feliz com o resultado, nao sei se ainda tem cheiro de café kkkk, o presente mais importante que fiz."
+    },
+
+    {
+      titulo: "Carta 2- Noites Brancas",
+      img: "imagens/objetos/carta2.jpeg",
+      texto: "A carta que eu coloquei no livro, junto com uma tentativa de desenhar o Banguela, sei que é simples, mas queria colocar algo que me lembrava de você."
     }
+
   ]
 
 };
